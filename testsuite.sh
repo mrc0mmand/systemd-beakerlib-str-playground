ac9b474abf29768eef779e62c582438202d46065
Frantisek Sumsal <fsumsal@redhat.com>
2019-03-01 17:24:25 +0100

    testsuite: cleanup artifact directories prior testing

--- testsuite.sh
+++ testsuite.sh
@@ -5,10 +5,16 @@ set -e
 . "$(dirname "$0")/setup-env.sh"
 
 ANSIBLE_ARGS="${ANSIBLE_ARGS:-}"
-TEST_ARTIFACTS="${TEST_ARTIFACTS:-$PWD/artifacts}"
+TEST_ARTIFACTS="${TEST_ARTIFACTS:-$PWD/artifacts-$(date --iso=minutes)}"
 
 if [[ ! -z $1 ]]; then
     ANSIBLE_ARGS+="-e \"fmf_filter='$1'\""
+fi
+
+# Cleanup artifact directories, so we get the most relevant test results
+rm -fr /tmp/artifacts
+if [[ -e $TEST_ARTIFACTS ]]; then
+    rm -fr "$TEST_ARTIFACTS"
 fi
 
 export TEST_ARTIFACTS
