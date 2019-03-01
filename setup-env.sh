33088a3f4335739bb75e62f97868ec4a4a9073ca
Frantisek Sumsal <fsumsal@redhat.com>
2019-03-01 16:46:30 +0100

    setup-env: get a full test root path instead of a relative one

--- setup-env.sh
+++ setup-env.sh
@@ -8,5 +8,5 @@ BEAKERLIB_LIB_BASE="/usr/share/beakerlib-libraries"
 [ ! -d "$BEAKERLIB_LIB_BASE" ] && mkdir -p "$BEAKERLIB_LIB_BASE"
 mkdir "$BEAKERLIB_LIB_BASE/systemd"
 
-TEST_REPO_ROOT="$(dirname "$0")"
+TEST_REPO_ROOT="$(dirname $(readlink -f "$0"))"
 ln -s "$TEST_REPO_ROOT/systemd/Library" "$BEAKERLIB_LIB_BASE/Library"
